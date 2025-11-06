5fc17f10f4d14e27d5fd308d5b099f2703cd2d15
Lipata <12410935@benilde.edu.ph>
2025-11-06 10:25:06 +0800

    techfest_registration.py

--- techfest_registration.py
+++ techfest_registration.py
@@ -30,25 +30,38 @@ for i, participants in enumerate(participants, start=1):
     print(f"{i}. {participants['name']} - {participants['track']}")
 
 
-#Part 2 Task 3
 participants = [
-    {"name": f"Ann", "track": "Cybersecurity"},
-    {"name": f"Rainier", "track": "Data"},
-    {"name": f"Leo", "track": "Game Dev"}
+    {"name": "Ana", "track": "Cybersecurity"},
+    {"name": "Mark", "track": "Data"},
+    {"name": "Ana", "track": "Cybersecurity"},
+    {"name": "Liza", "track": "Data"},
+    {"name": "Ana", "track": "Cybersecurity"},
+
 ]
+
 
 offer_tracks = {p["track"] for p in participants}
 
 if len(offer_tracks) < 2:
     print("Not enough variety in tracks.")
 else:
-    print("Tracks offered in this event: ".join(offer_tracks))
+    print("Tracks offered in this event:", ", ".join(offer_tracks))
 
-print(f"Duplicate name found {len(participants)} participants.")
 
-print("Participants per track: ")
-for i, participants in enumerate(participants, start=1):
-    print(f"{i}. {participants['name']} - {participants['track']}")
+names = [p["name"] for p in participants]
+duplicates = set([name for name in names if names.count(name) > 1])
+
+if duplicates:
+    for name in duplicates:
+        print(f"\nDuplicate name found: {name}")
+
+from collections import Counter
+
+track_counts = Counter(p["track"] for p in participants)
+
+print("\nParticipants per track:")
+for track, count in track_counts.items():
+    print(f"{track}: {count}")
 
 
 
